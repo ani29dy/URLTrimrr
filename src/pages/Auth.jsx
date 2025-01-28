53dff1171b075e3ee86c5f8d643693898f8d301c
ani29dy <aniketanni2916@gmail.com>
2025-01-28 18:51:55 +0530

    Created LandingPage and AuthorizationPage

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+  const [searchParams] = useSearchParams();
 
-export default Auth
+  return (
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">
+        {searchParams.get("createNew")
+          ? "Hold up! Let's login first"
+          : "Login / Signup"}
+      </h1>
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">Login Component</TabsContent>
+        <TabsContent value="signup">Signup Component</TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Auth;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,18 +1,77 @@
-import React from "react";
+import { React, useState } from "react";
 import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
+import { Button, buttonVariants } from "../components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const [longURL, setLongURL] = useState();
+  const navigate = useNavigate();
+
+  const handleShorten = (e) => {
+    e.preventDefault();
+    if (longURL) navigate(`/auth?createNew=${longURL}`);
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7x1 text-white text-center font-extrabold">
         The only URL Shortener <br /> you&rsquo;ll ever need! 👇
       </h2>
-      <form className="sm:h-14 flex flex-col w-full sm:flex-row md:w-2/4 gap-2">
-        <Input />
-        <Button className="bg-white text-black cursor-pointer">Shorten!</Button>
+      <form
+        onSubmit={handleShorten}
+        className="sm:h-14 flex flex-row sm:flex-col w-full md:w-2/4 gap-2"
+      >
+        <Input
+          type="url"
+          value={longURL}
+          onChange={(e) => setLongURL(e.target.value)}
+          placeholder="Enter your loong URL"
+          className="flex-1 h-full py-4 px-4"
+        />
+        <Button
+          className="h-full bg-red-900"
+          type="submit"
+          variant="destructive"
+        >
+          Shorten!
+        </Button>
       </form>
       <img src="banner2.jpg" alt="Banner" className="w-full md:px-11 my-11" />
+
+      <Accordion type="multiple" collapsible="true" className="w-full md:px-11">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>How does the URL Shortener works?</AccordionTrigger>
+          <AccordionContent>
+            When you enter a long URL, our system generates a shorter version of
+            that URL. This shortened URL redirects to the original URL when
+            visited.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            Do I need an account to use the app?
+          </AccordionTrigger>
+          <AccordionContent>
+            Yes. Creating an account allows you to manage your shortened URLs,
+            view analytics, and customize your short URLs.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+            What analytics are available for my shortened URLs?
+          </AccordionTrigger>
+          <AccordionContent>
+            You can view the number of clicks, geolocation data of the clicks
+            and device types(Mobile/Desktop) for each of your shortened URLs.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 };
