087c9f2f5ec17a1937f05fa4e607e9cac58fc607
ani29dy <aniketanni2916@gmail.com>
2025-02-02 15:40:14 +0530

    Added Logout function

--- src/db/APIAuth.js
+++ src/db/APIAuth.js
@@ -11,6 +11,36 @@ export const login = async ({ email, password }) => {
   return data;
 };
 
+export async function signup({ name, email, password, profile_pic }) {
+  const fileName = `dp-${name.split(" ").join("-")}-${Math.random()}`;
+
+  const { data: uploadData, error: storageError } = await supabase.storage
+    .from("profile_pic")
+    .upload(fileName, profile_pic);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const { data: publicUrlData } = supabase.storage
+    .from("profile_pic")
+    .getPublicUrl(fileName);
+
+  if (!publicUrlData) throw new Error("Failed to get public URL");
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        name,
+        profile_pic: publicUrlData.publicUrl,
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
+
 export async function getCurrentUser() {
   const { data: session, error } = await supabase.auth.getSession();
   if (!session.session) return null;
@@ -18,24 +48,7 @@ export async function getCurrentUser() {
   return session.session?.user;
 }
 
-export async function signup({ name, email, password, profile_pic }) {
-  const fileName = `dp-${name.split(" ").join("-")} - ${Math.random()}`;
-  const { error: storageError } = await supabase.storage
-    .from("profile_pic")
-    .upload(fileName, profile_pic);
-
-  if (storageError) throw new Error(error.message);
-
-  await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      data: {
-        name,
-        profile_pic: `${supabase}/storage/v1/object/public/profile_pic/${fileName}`,
-      },
-    },
-  });
-
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
 }
