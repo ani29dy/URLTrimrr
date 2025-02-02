033aec10f9eee27e2ccf7ab7564d10c2e0056987
ani29dy <aniketanni2916@gmail.com>
2025-02-02 15:45:57 +0530

    Updated to display the User details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,48 +11,73 @@ import {
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { LinkIcon, LogOut } from "lucide-react";
+import { UrlState } from "../Context";
+import useFetch from "../hooks/useFetch";
+import { logout } from "../db/APIAuth";
+import { BarLoader } from "react-spinners";
+
 const Header = () => {
   const navigate = useNavigate();
 
-  const user = true;
+  const { user, fetchUser } = UrlState();
+
+  const { loading, fn: fnLogout } = useFetch(logout);
 
   return (
-    <nav className="flex justify-between items-center py-4">
-      <Link to="/">
-        <img src="./logo.png" className="h-16" alt="Trimrr Logo" />
-      </Link>
-      <div>
-        {!user ? (
-          <Button
-            onClick={() => navigate("/auth")}
-            className="bg-white text-black cursor-pointer"
-          >
-            Login
-          </Button>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Ani_2916</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <LinkIcon className="mt-0.5 h-4 w-4" />
-                <span>My Links</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="text-red-800">
-                <LogOut className="mt-0.5 h-4 w-4" />
-                <span>Logout</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-    </nav>
+    <>
+      <nav className="flex justify-between items-center py-4">
+        <Link to="/">
+          <img src="./logo.png" className="h-16" alt="Trimrr Logo" />
+        </Link>
+        <div>
+          {!user ? (
+            <Button
+              onClick={() => navigate("/auth")}
+              className="bg-white text-black cursor-pointer"
+            >
+              Login
+            </Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+                <Avatar>
+                  <AvatarImage
+                    src={user?.user_metadata?.profile_pic}
+                    className="object-contain"
+                  />
+                  <AvatarFallback>PP</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  {user?.user_metadata?.name}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to={"/dashboard"} className="flex">
+                    <LinkIcon className="mt-0.5 h-4 w-4" />
+                    <span>My Links</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-red-800">
+                  <LogOut className="mt-0.5 h-4 w-4" />
+                  <span
+                    onClick={() => {
+                      fnLogout().then(() => {
+                        navigate("/");
+                      });
+                    }}
+                  >
+                    Logout
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>
+      {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+    </>
   );
 };
 
