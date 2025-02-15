88594caadb9afef707aa27b54114f8397cb5bad3
ani29dy <aniketanni2916@gmail.com>
2025-02-15 12:09:09 +0530

    Created RedirectLink Page

--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { StoreClicks } from "../db/APIClicks";
+import { BarLoader } from "react-spinners";
+import { getLongUrl } from "../db/APIUrls";
 
 const RedirectLink = () => {
-  return (
-    <div>RedirectLink</div>
-  )
-}
+  const { id } = useParams();
 
-export default RedirectLink
+  const { loading, data, fn } = useFetch(getLongUrl, id);
+
+  const { loading: loadingStats, fn: fnStats } = useFetch(StoreClicks, {
+    id: data?.id,
+    originalUrl: data?.original_url,
+  });
+
+  useEffect(() => {
+    fn();
+  }, []);
+
+  useEffect(() => {
+    if (!loading && data) {
+      fnStats();
+    }
+  }, [loading]);
+
+  if (loading && loadingStats) {
+    return (
+      <>
+        <BarLoader width={"100%"} color="#36d7b7" />
+        <br />
+        Redirecting.....
+      </>
+    );
+  }
+
+  return null;
+};
+
+export default RedirectLink;
