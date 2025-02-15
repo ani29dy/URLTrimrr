69b4822905f6e53356399d80a80236250e77d387
ani29dy <aniketanni2916@gmail.com>
2025-02-15 12:08:30 +0530

    Updated APIClicks

--- src/db/APIClicks.js
+++ src/db/APIClicks.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase from "./supabase";
 
 export async function getClicksForUrls(url_id) {
@@ -8,6 +9,41 @@ export async function getClicksForUrls(url_id) {
   if (error) {
     console.error(error.message);
     throw new Error("Unable to load URLs");
+  }
+  return data;
+}
+
+const parser = new UAParser();
+
+export const StoreClicks = async ({ id, originalUrl }) => {
+  try {
+    const res = parser.getResult();
+    const device = res.type || "Desktop";
+
+    const response = await fetch("https://ipapi.co/json");
+    const { city, country_name: country } = await response.json();
+
+    await supabase.from("clicks").insert({
+      url_id: id,
+      city: city,
+      country: country,
+      device: device,
+    });
+
+    window.location.href = originalUrl;
+  } catch (error) {
+    console.log("Error recording Click:", error);
+  }
+};
+
+export async function getClicksForUrl(url_id) {
+  const { data, error } = await supabase
+    .from("clicks")
+    .select("*")
+    .eq("url_id", url_id);
+  if (error) {
+    console.error(error.message);
+    throw new Error("Unable to Load Stats");
   }
   return data;
 }
