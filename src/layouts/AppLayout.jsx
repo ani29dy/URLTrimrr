8161e55ffc9a18af42c7438384454e45eb633f9b
ani29dy <aniketanni2916@gmail.com>
2025-02-20 13:31:26 +0530

    AppLayout modified

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -6,15 +6,12 @@ const AppLayout = () => {
   return (
     <div>
       <main className="min-h-screen container px-10">
-        {/* Header */}
         <Header />
-        {/* Body */}
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
         Made with ❤️ by Ani!!!
       </div>
-      {/* Footer */}
     </div>
   );
 };
