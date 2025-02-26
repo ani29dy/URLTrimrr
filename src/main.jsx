f8d33dd70c0131c4b9ec87fd25509545325c0846
ani29dy <aniketanni2916@gmail.com>
2025-02-26 13:36:07 +0530

    Main.jsx Modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import UrlProvider from "./Context.jsx";
+import React from "react";
 
 createRoot(document.getElementById("root")).render(
-  <UrlProvider>
+  <React.StrictMode>
     <App />
-  </UrlProvider>
+  </React.StrictMode>
 );
